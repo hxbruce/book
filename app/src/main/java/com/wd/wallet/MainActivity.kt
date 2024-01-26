d713d6309f1713c6663aa50d787c0e3b61b24585
bruce <bruce@gmail.com>
2024-01-26 15:06:06 +0800

    q

--- app/src/main/java/com/wd/wallet/MainActivity.kt
+++ app/src/main/java/com/wd/wallet/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val projectId = "" // Get Project ID at https://cloud.walletconnect.com/
+        val projectId = "89b957fac364f290134f68c547f516e1" // Get Project ID at https://cloud.walletconnect.com/
         val relayUrl = "relay.walletconnect.com"
         val serverUrl = "wss://$relayUrl?projectId=$projectId"
         val connectionType = ConnectionType.AUTOMATIC
@@ -27,8 +27,8 @@ class MainActivity : AppCompatActivity() {
             icons =listOf("R.mipmap.ic_launcher")/*list of icon url strings*/,
             redirect = "kotlin-wallet-wc:/request" // Custom Redirect URI
         )
-
-        CoreClient.initialize(relayServerUrl = serverUrl, connectionType = connectionType, application = this, metaData = appMetaData)
+//爆红
+//        CoreClient.initialize(metaData = appMetaData,relayServerUrl = serverUrl, connectionType = connectionType, application = this )
 
         val initParams = Wallet.Params.Init(core = CoreClient)
 
@@ -37,11 +37,57 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+        val anotherSupportedNamespaces = mapOf(
+            "eip155" to Wallet.Model.Namespace.Session(
+                chains = listOf("eip155:1", "eip155:2", "eip155:4"),
+                methods = listOf("personal_sign", "eth_sendTransaction", "eth_signTransaction"),
+                events = listOf("chainChanged", "accountsChanged"),
+                accounts = listOf("eip155:1:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092", "eip155:2:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092", "eip155:4:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092")
+            ),
+            "cosmos" to Wallet.Model.Namespace.Session(
+                chains = listOf("cosmos:cosmoshub-4"),
+                methods = listOf("cosmos_method"),
+                events = listOf("cosmos_event"),
+                accounts = listOf("cosmos:cosmoshub-4:cosmos1hsk6jryyqjfhp5dhc55tc9jtckygx0eph6dd02")
+            )
+        )
+        val supportedNamespace = mapOf(
+            "eip155" to Wallet.Model.Namespace.Session(
+                chains = listOf("eip155:1", "eip155:137", "eip155:3"),
+                methods = listOf("personal_sign", "eth_sendTransaction", "eth_signTransaction"),
+                events = listOf("chainChanged"),
+                accounts = listOf("eip155:1:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092", "eip155:137:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092", "eip155:3:0x57f48fAFeC1d76B27e3f29b8d277b6218CDE6092")
+            )
+        )
+        // 在 onSessionProposal 方法中
+        fun onSessionProposal(sessionProposal: Wallet.Model.SessionProposal) {
+            // 处理 sessionProposal，其他逻辑...
+
+        }
+
+// 外部变量声明
+        var sessionProposa: Wallet.Model.SessionProposal? = null
+
+// 在需要使用 sessionProposal 的地方
+        val sessionProposalInstance: Wallet.Model.SessionProposal? = sessionProposa
+
+        val supportedNamespaces: Map<String, Wallet.Model.Namespace.Session> =anotherSupportedNamespaces /* a map of all supported namespaces created by a wallet */
+        val sessionProposal: Wallet.Model.SessionProposal? = sessionProposalInstance /* an object received by `fun onSessionProposal(sessionProposal: Wallet.Model.SessionProposal)` in `Web3Wallet.WalletDelegate` */
+        val sessionNamespaces =
+            sessionProposal?.let { Web3Wallet.generateApprovedNamespaces(it, supportedNamespaces) }
+//爆红
+//        val approveParams: Wallet.Params.SessionApprove = Wallet.Params.SessionApprove(proposerPublicKey, sessionNamespaces)
+//        Web3Wallet.approveSession(approveParams) { error -> /*callback for error while approving a session*/ }
+
+
+
+
+
 
 
     }
 }
 
-private fun CoreClient.initialize(relayServerUrl: String, connectionType: ConnectionType, application: MainActivity, metaData: Core.Model.AppMetaData) {
 
-}
+
+
